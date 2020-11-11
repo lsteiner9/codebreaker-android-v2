bd2221bb6e660bebb18036869f158116a3ef4db3
lsteiner9 <lsteiner1@cnm.edu>
2020-11-11 13:12:30 -0700

    Fixed unused imports.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/CodeCharacterAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/CodeCharacterAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import edu.cnm.deepdive.codebreaker.R;
